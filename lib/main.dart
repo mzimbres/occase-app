c4b57fd993f5434ddf86dd0315e2db405068c6c1
Marcelo Zimbres <mzimbres@gmail.com>
2018-12-09 10:06:33 +0100

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+Future<Null> main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MenuChat",
+      theme: ThemeData(
+        primaryColor: Color(0xff075E54),
+        accentColor: Color(0xff25D366),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WhatsAppHome(),
+    );
+  }
+}
+
+class Menu extends StatefulWidget {
+  @override
+  MenuState createState() => new MenuState();
+}
+
+class MenuState extends State<Menu> {
+  List<Widget> _marcas = <Widget>[
+                RaisedButton( child: Text("Alfa Romeo")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("BMW")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Fiat")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Volkswagen")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Mercedes")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Ford")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Chevrolet")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Citroen")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Peugeot")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Ferrari")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Dodge")
+                            , onPressed: () => print("pressed")),
+                RaisedButton( child: Text("Rolls Roice")
+                            , onPressed: () => print("pressed")),
+             ];
+
+  //@override
+  //void initState() {
+  //  super.initState();
+  //}
+
+  @override
+  Widget build(BuildContext context) {
+     return ListView(
+                 shrinkWrap: true,
+                 padding: const EdgeInsets.all(20.0),
+                 children: this._marcas
+          );
+  }
+}
+
+class WhatsAppHome extends StatefulWidget {
+  WhatsAppHome();
+
+  @override
+  _WhatsAppHomeState createState() => new _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  List<Widget> marcas = <Widget>[
+             RaisedButton( child: Text("Alfa Romeo")
+                         , onPressed: () => print("pressed")),
+             RaisedButton( child: Text("BMW")
+                         , onPressed: () => print("pressed")),
+             RaisedButton( child: Text("Fiat")
+                         , onPressed: () => print("pressed")),
+          ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = new TabController(vsync: this, initialIndex: 1, length: 3);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FipeChat"),
+        elevation: 0.7,
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(text: "ANUNCIOS"),
+            Tab( text: "TABELA",),
+            Tab( text: "CHATS",),
+          ],
+        ),
+        actions: <Widget>[
+          Icon(Icons.search),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+          Icon(Icons.more_vert)
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Tab(text: "Anuncios"),
+          Menu(),
+          Tab(text: "Chat list"),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: Icon(
+          Icons.message,
+          color: Colors.white,
+        ),
+        onPressed: () => print("open chats"),
+      ),
+    );
+  }
+}
+
