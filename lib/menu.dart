a8873569ae65fdc958a897a288d904560ced98ef
Marcelo Zimbres <mzimbres@gmail.com>
2018-12-10 23:32:04 +0100

    Progresses.

--- lib/menu.dart
+++ lib/menu.dart
@@ -16,27 +16,33 @@ class BrandItem extends StatelessWidget {
 }
 
 class MenuScreen extends StatelessWidget {
-   final List<Widget> _brands = new List<Widget>();
-   MenuScreen(List<String> items)
+   List<String> _items;
+   MenuScreen(this._items);
+
+   @override
+   Widget build(BuildContext context)
    {
-      //this._brands = new List<Widget>();
-      for (String o in items) {
-         this._brands.add(RaisedButton(
+      List<Widget> _brands = new List<Widget>();
+      for (String o in this._items) {
+         _brands.add(RaisedButton(
                      child: BrandItem(o),
-                     //onPressed: update,
+                     onPressed: () {
+                        print("pressed3");
+                        Navigator.of(context).push( MaterialPageRoute(builder: (context) => MenuScreen(<String>[
+                                 "Uno mil",
+                                 "Palio",
+                              ])),
+                        );
+                     },
                      color: const Color(0xFFFFFF),
                      highlightColor: const Color(0xFFFFFF)
          ));
       }
-   }
-
-   @override
-   Widget build(BuildContext context) {
       return ListView(
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.all(20.0),
-                    children: this._brands
-              );
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(20.0),
+            children: _brands
+      );
    }
 }
 
@@ -73,33 +79,33 @@ class MenuState extends State<Menu> {
   void initState()
   {
      super.initState();
-     _marcas = new List<Widget>();
-     for (String o in _brands) {
-        _marcas.add(RaisedButton(
-                    child: BrandItem(o),
-                    onPressed: update,
-                    color: const Color(0xFFFFFF),
-                    highlightColor: const Color(0xFFFFFF)
-        ));
-     }
+     //_marcas = new List<Widget>();
+     //for (String o in _brands) {
+     //   _marcas.add(RaisedButton(
+     //               child: BrandItem(o),
+     //               onPressed: update,
+     //               color: const Color(0xFFFFFF),
+     //               highlightColor: const Color(0xFFFFFF)
+     //   ));
+     //}
      //_marcas = _brands;
   }
 
-  void update()
-  {
-     setState(() {
-        //_marcas.clear();
-        for (String o in _models) {
-           _marcas.add(RaisedButton(
-                       child: BrandItem(o),
-                       onPressed: () {print("nada");},
-                       color: const Color(0xFFFFFF),
-                       highlightColor: const Color(0xFFFFFF)
-           ));
-        }
-        print("pressed2");
-     });
-  }
+  //void update()
+  //{
+  //   setState(() {
+  //      //_marcas.clear();
+  //      //for (String o in _models) {
+  //      //   _marcas.add(RaisedButton(
+  //      //               child: BrandItem(o),
+  //      //               onPressed: () {print("nada");},
+  //      //               color: const Color(0xFFFFFF),
+  //      //               highlightColor: const Color(0xFFFFFF)
+  //      //   ));
+  //      //}
+  //      //print("pressed2");
+  //   });
+  //}
 
   @override
   Widget build(BuildContext context) {
