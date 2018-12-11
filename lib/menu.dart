534e2a0e1e9f97fef9f1662eb3916b9beeae016b
Marcelo Zimbres <mzimbres@gmail.com>
2018-12-11 22:22:29 +0100

    Minor changes.

--- lib/menu.dart
+++ lib/menu.dart
@@ -10,6 +10,7 @@ class BrandItem extends StatelessWidget {
     return new ListTile(
         leading: new CircleAvatar(child: new Text("M")),
         title: new Text(_brand),
+        dense: true,
         //subtitle: new Text("What can we do")
         );
   }
@@ -35,7 +36,7 @@ class MenuScreen extends StatelessWidget {
                         );
                      },
                      color: const Color(0xFFFFFF),
-                     highlightColor: const Color(0xFFFFFF)
+                     //highlightColor: const Color(0xFFFFFF)
          ));
       }
       return ListView(
@@ -52,88 +53,66 @@ class Menu extends StatefulWidget {
 }
 
 class MenuState extends State<Menu> {
-  List<Widget> _marcas;
-  List<String> _brands = <String>[
-                "Alfa Romeo",
-                "BMW",
-                "Fiat",
-                "Volkswagen",
-                "Mercedes",
-                "Ford",
-                "Chevrolet",
-                "Citroen",
-                "Peugeot",
-                "Ferrari",
-                "Dodge",
-                "Rolls Roice",
-             ];
+   List<Widget> _marcas;
+   List<String> _brands = <String>[
+      "Alfa Romeo",
+      "BMW",
+      "Fiat",
+      "Volkswagen",
+      "Mercedes",
+      "Ford",
+      "Chevrolet",
+      "Citroen",
+      "Peugeot",
+      "Ferrari",
+      "Dodge",
+      "Rolls Roice",
+   ];
 
-  List<String> _models = <String>[
-     "Uno mil",
-     "Palio",
-  ];
+   List<String> _models = <String>[
+      "Uno mil",
+      "Palio",
+   ];
 
-  int _selectedIndex = 1;
+   int _selectedIndex = 1;
 
-  @override
-  void initState()
-  {
-     super.initState();
-     //_marcas = new List<Widget>();
-     //for (String o in _brands) {
-     //   _marcas.add(RaisedButton(
-     //               child: BrandItem(o),
-     //               onPressed: update,
-     //               color: const Color(0xFFFFFF),
-     //               highlightColor: const Color(0xFFFFFF)
-     //   ));
-     //}
-     //_marcas = _brands;
-  }
+   @override
+   void initState()
+   {
+      super.initState();
+   }
 
-  //void update()
-  //{
-  //   setState(() {
-  //      //_marcas.clear();
-  //      //for (String o in _models) {
-  //      //   _marcas.add(RaisedButton(
-  //      //               child: BrandItem(o),
-  //      //               onPressed: () {print("nada");},
-  //      //               color: const Color(0xFFFFFF),
-  //      //               highlightColor: const Color(0xFFFFFF)
-  //      //   ));
-  //      //}
-  //      //print("pressed2");
-  //   });
-  //}
+   List<MenuScreen> screens;
 
-  @override
-  Widget build(BuildContext context) {
-     return Scaffold(
-           body: MenuScreen(this._brands),
-           //body: ListView(
-           //      shrinkWrap: true,
-           //      padding: const EdgeInsets.all(20.0),
-           //      children: this._marcas
-           //),
-           bottomNavigationBar: BottomNavigationBar(
-               items: <BottomNavigationBarItem>[
-               BottomNavigationBarItem(
-                     icon: Icon(Icons.home), title: Text('Localizacao')),
-               BottomNavigationBarItem(
-                     icon: Icon(Icons.business), title: Text('Modelos')),
-           ],
-           currentIndex: _selectedIndex,
-           fixedColor: Colors.deepPurple,
-           onTap: _onItemTapped,
-           ),
-           );
-  }
+   MenuState()
+   {
+      screens = new List<MenuScreen>();
+      screens.add(MenuScreen(_brands));
+      screens.add(MenuScreen(_models));
+   }
+
+   @override
+   Widget build(BuildContext context) {
+      return Scaffold(
+            body: MenuScreen(this._brands),
+            bottomNavigationBar: BottomNavigationBar(
+                  items: <BottomNavigationBarItem>[
+                     BottomNavigationBarItem(
+                           icon: Icon(Icons.home), title: Text('Localizacao')),
+                     BottomNavigationBarItem(
+                           icon: Icon(Icons.business), title: Text('Modelos')),
+                  ],
+                  currentIndex: _selectedIndex,
+                  fixedColor: Colors.deepPurple,
+                  onTap: _onItemTapped,
+            ),
+      );
+   }
    void _onItemTapped(int index) {
-   setState(() {
-     _selectedIndex = index;
-   });
- }
+      setState(() {
+         _selectedIndex = index;
+      });
+   }
 }
 
 
