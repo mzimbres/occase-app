44068e0a943c33fb4116da53465c76ba2c6abb71
Marcelo Zimbres <mzimbres@gmail.com>
2018-12-12 21:59:38 +0100

    More progress.

--- lib/menu.dart
+++ lib/menu.dart
@@ -16,24 +16,54 @@ class BrandItem extends StatelessWidget {
   }
 }
 
-class MenuScreen extends StatelessWidget {
-   List<String> _items;
-   MenuScreen(this._items);
+class MenuNode {
+   String name;
+   List<MenuNode> children;
+}
+
+class MenuTree {
+   MenuNode root;
+   List<MenuNode> st;
+}
+
+class MenuScreen extends StatefulWidget {
+  List<List<String>> _items;
+  int _index;
+  MenuScreen(this._items, this._index);
+  @override
+  MenuScreenState createState() => new MenuScreenState(_items, _index);
+}
+
+class MenuScreenState extends State<MenuScreen> {
+   List<List<String>> _items;
+   int _index;
+   MenuScreenState(this._items, this._index);
+
+   @override
+   void initState()
+   {
+      super.initState();
+   }
 
    @override
    Widget build(BuildContext context)
    {
       List<Widget> _brands = new List<Widget>();
-      for (String o in this._items) {
+      for (String o in this._items[_index]) {
          _brands.add(RaisedButton(
                      child: BrandItem(o),
                      onPressed: () {
-                        print("pressed3");
-                        Navigator.of(context).push( MaterialPageRoute(builder: (context) => MenuScreen(<String>[
-                                 "Uno mil",
-                                 "Palio",
-                              ])),
-                        );
+                        if (_index == 0) {
+                           Navigator.of(context).push(
+                                 MaterialPageRoute(
+                                       builder: (context) {
+                                          return MenuScreen(_items, 1);
+                                       }
+                                 ),
+                           );
+                        } else {
+                           Navigator.pop(context);
+                        }
                      },
                      color: const Color(0xFFFFFF),
                      //highlightColor: const Color(0xFFFFFF)
@@ -54,28 +84,7 @@ class Menu extends StatefulWidget {
 
 class MenuState extends State<Menu> {
    List<Widget> _marcas;
-   List<String> _brands = <String>[
-      "Alfa Romeo",
-      "BMW",
-      "Fiat",
-      "Volkswagen",
-      "Mercedes",
-      "Ford",
-      "Chevrolet",
-      "Citroen",
-      "Peugeot",
-      "Ferrari",
-      "Dodge",
-      "Rolls Roice",
-   ];
-
-   List<String> _models = <String>[
-      "Uno mil",
-      "Palio",
-   ];
-
    List<MenuScreen> screens;
-
    int _selectedIndex = 1;
 
    @override
@@ -87,25 +96,48 @@ class MenuState extends State<Menu> {
 
    MenuState()
    {
+      List<String> _brands = <String>[
+         "Alfa Romeo",
+         "BMW",
+         "Fiat",
+         "Volkswagen",
+         "Mercedes",
+         "Ford",
+         "Chevrolet",
+         "Citroen",
+         "Peugeot",
+         "Ferrari",
+         "Dodge",
+         "Rolls Roice",
+      ];
+
+      List<String> _models = <String>[
+         "Uno mil",
+         "Palio",
+         "Premio",
+      ];
+
+      List<List<String>> a1 = new List<List<String>>();
+      a1.add(_brands);
+      a1.add(_models);
+
       screens = new List<MenuScreen>();
-      screens.add(MenuScreen(_brands));
-      screens.add(MenuScreen(_models));
+      screens.add(MenuScreen(a1, 0));
+      screens.add(MenuScreen(a1, 0));
    }
 
    @override
    Widget build(BuildContext context) {
       return Scaffold(
-
-            //body: MenuScreen(this._brands),
             body: new Stack(
-                    children: new List<Widget>.generate(screens.length, (int index) {
-                      return new IgnorePointer(
+                    children: List<Widget>.generate(screens.length, (int index) {
+                      return IgnorePointer(
                         ignoring: index != _selectedIndex,
-                        child: new Opacity(
+                        child: Opacity(
                           opacity: _selectedIndex == index ? 1.0 : 0.0,
-                          child: new Navigator(
+                          child: Navigator(
                             onGenerateRoute: (RouteSettings settings) {
-                              return new MaterialPageRoute(
+                              return MaterialPageRoute(
                                 builder: (_) => screens[index],
                               );
                             },
