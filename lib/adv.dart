7cfdf75cd405771541b308367692fd500c3c9b08
Marcelo Zimbres <mzimbres@gmail.com>
2018-12-16 14:32:11 +0100

    Advertisement class improvements.

--- lib/adv.dart
+++ lib/adv.dart
@@ -8,15 +8,6 @@ class Adv extends StatefulWidget {
 
 RichText createText(BuildContext context, String key, String value)
 {
-   //return Text(str,
-   //      textAlign: TextAlign.left,
-   //      overflow: TextOverflow.ellipsis,
-   //      style: TextStyle(
-   //            fontWeight: FontWeight.bold,
-   //            fontSize: Consts.mainFontSize
-   //      )
-   //);
-
    return RichText(
          text: TextSpan(
                text: key,
@@ -29,15 +20,18 @@ RichText createText(BuildContext context, String key, String value)
    );
 }
 
-List<RichText> headerFactory(BuildContext context)
+Column headerFactory(BuildContext context)
 {
    List<RichText> r = List<RichText>();
-   r.add(createText(context, "Marca",      ": Volkswagen"));
-   r.add(createText(context, "Modelo",     ": Brasilia"));
-   r.add(createText(context, "Ano",        ": 1985/86"));
-   r.add(createText(context, "Preco Fipe", ": 1200"));
-   r.add(createText(context, "Anunciante", ": Paulinho Nacimento"));
-   return r;
+   r.add(createText(context, "Marca: ",      "Volkswagen"));
+   r.add(createText(context, "Modelo: ",     "Brasilia"));
+   r.add(createText(context, "Ano: ",        "1985/86"));
+   r.add(createText(context, "Preco Fipe: ", "1200"));
+   r.add(createText(context, "Anunciante: ", "Paulinho Nacimento"));
+
+   return Column( crossAxisAlignment: CrossAxisAlignment.stretch,
+         children: r,
+   );
 }
 
 class AdvState extends State<Adv> {
@@ -51,23 +45,43 @@ class AdvState extends State<Adv> {
   @override
   Widget build(BuildContext context)
   {
-     final Card header1 = new Card(
-           child: Column(
-                 crossAxisAlignment: CrossAxisAlignment.stretch,
-                 children: headerFactory(context),
-           ),
+     Card c1 = Card(
+           child: headerFactory(context),
+           color: Colors.teal[100],
+           margin: EdgeInsets.all(5.0),
+           elevation: 0.0,
      );
 
-     final Card header2 = new Card(
-           child: new Column(
-                 crossAxisAlignment: CrossAxisAlignment.stretch,
-                 children: headerFactory(context),
-           ),
+     String msg = "Carro em bom estado de conservacao. Único dono.";
+            msg += "Documentos em dia (Paulinho garante).";
+
+     Card c2 = Card(
+           child: createText(context, "Descricao: ", msg),
+           color: Colors.teal[300],
+           margin: EdgeInsets.all(5.0),
+           elevation: 0.0,
      );
 
-     final List<Card> cards = new List<Card>();
-     cards.add(header1);
-     cards.add(header2);
+     Column h1 = Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+           children: <Card>[c1, c2],
+     );
+
+     Card adv1 = Card(
+           child: h1,
+           color: Colors.teal[300],
+           margin: EdgeInsets.all(5.0),
+           elevation: 0.0,
+     );
+
+     final List<Card> cards = List<Card>();
+     cards.add(adv1);
+     cards.add(adv1);
+     cards.add(adv1);
+     cards.add(adv1);
+     cards.add(adv1);
+     cards.add(adv1);
+     cards.add(adv1);
+     cards.add(adv1);
 
      return Scaffold(body:ListView(
                  shrinkWrap: true,
