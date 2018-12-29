d6318f73142ca97bfc787ffcd3dc2990045cc9d0
Marcelo Zimbres <mzimbres@gmail.com>
2018-12-29 19:42:03 +0100

    Parser of the new file format working.

--- lib/menu_tree.dart
+++ lib/menu_tree.dart
@@ -34,12 +34,13 @@ MenuNode buildTree(String msg)
    Map<String, dynamic> menu = jsonDecode(msg);
    String dataRaw = menu["data"];
    List<String> data = dataRaw.split("=");
+   print("Number of lines: ${data.length}");
    List<MenuNode> st = List<MenuNode>();
    int last_depth = 0;
    MenuNode root;
    for (String line in data) {
       if (line.isEmpty)
-         return null;
+         continue;
 
       List<String> fields = line.split(";");
 
@@ -53,8 +54,9 @@ MenuNode buildTree(String msg)
       }
 
       if (depth > last_depth) {
-         if (last_depth + 1 != depth)
+         if (last_depth + 1 != depth) {
             return null;
+         }
 
          // We found the child of the last node pushed on the stack.
          MenuNode p = MenuNode(name);
@@ -85,32 +87,22 @@ MenuNode buildTree(String msg)
          // Last depth stays equal.
       }
 
-      print("Depth: $depth. Name: " + name);
+      //print("Depth: $depth. Name: " + name);
    }
 
    return root;
+}
 
-   //while (arr != null) {
-      //for (var o in arr) {
-      //   Map<String, dynamic> sub = o;
-      //   MenuNode node = MenuNode(sub["name"], sub["hash"]);
-      //   //rootNode.children.add(node);
-      //   print("${node.name} ===> ${node.code}");
-      //}
-   //}
-
-   //root.forEach((k, v) => print(v));
-
-
-   //Map<String, dynamic> root = jsonDecode(arr.first);
-   //var root = jsonDecode(arr.first);
-   //print("Root sub size: ${root.length}");
+List<MenuNode> ModelsFactory()
+{
+   MenuNode root2 = buildTree(Consts.menu);
+   List<MenuNode> ret2 = List<MenuNode>();
+   ret2.add(root2);
+   return ret2;
 }
 
 List<MenuNode> LocationFactory()
 {
-   buildTree(Consts.menu);
-
    MenuNode root = MenuNode("Brasil", "");
    root.children.add(makeNode("Acre", <String>["Porto Velho", "Cruzeiro do Sul", "Sena Madureira", "Tarauacá", "Feijó", "Outras"]));
    root.children.add(makeNode("Alagoas", <String>["Maceio", "Arapiraca", "Rio Largo", "Palmeira dos Índios", "União dos Palmares", "Outras"]));
@@ -143,53 +135,3 @@ List<MenuNode> LocationFactory()
    return ret;
 }
 
-List<MenuNode> ModelsFactory()
-{
-   MenuNode root = MenuNode("Brasil", "");
-   root.children.add(makeNode("Acura", <String>["Porto Velho", "Cruzeiro do Sul", "Sena Madureira", "Tarauacá", "Feijó", "Outras"]));
-   root.children.add(makeNode("Agrale", <String>["Maceio", "Arapiraca", "Rio Largo", "Palmeira dos Índios", "União dos Palmares", "Outras"]));
-   root.children.add(makeNode("Alpha Romeo", <String>["Macapa", "Santana", "Laranjal do Jari", "Oiapoque", "Mazagão", "Outras"]));
-   root.children.add(makeNode("AM Gen", <String>["Manaus", "Parintins", "Itacoatiara", "Manacapuru", "Coari", "Outras"]));
-   root.children.add(makeNode("Asia Motors", <String>["Salvador", "Feira de Santana", "Vitória da Conquista", "Camaçari", "Itabuna", "Outras"]));
-   root.children.add(makeNode("Aston Martin", <String>["Fortaleza", "Caucaia", "Juazeiro do Norte", "Maracanaú", "Sobral", "Outras"]));
-   root.children.add(makeNode("Audi", <String>["Brasilia"]));
-   root.children.add(makeNode("Baby", <String>["Vitoria", "Vila Velha", "Cariacica", "Serra", "Outras"]));
-   root.children.add(makeNode("BMW", <String>["Goiânia", "Outras"]));
-   root.children.add(makeNode("BRM", <String>["Sao Luis", "Outras"]));
-   root.children.add(makeNode("Bugre", <String>["Cuiaba", "Outras"]));
-   root.children.add(makeNode("Cadilac", <String>["Campo Grande", "Outras"]));
-   root.children.add(makeNode("CBT Jipe", <String>["Belo Horizonte", "Outras"]));
-   root.children.add(makeNode("Chana", <String>["Belém", "Outras"]));
-   root.children.add(makeNode("Changan", <String>["Joao Pessoa", "Outras"]));
-   root.children.add(makeNode("Chery", <String>["Recife", "Outras"]));
-   root.children.add(makeNode("Chrysler", <String>["Terizina", "Outras"]));
-   root.children.add(makeNode("Citroen", <String>["Rio de Janeiro", "Outras"]));
-   root.children.add(makeNode("Cross Lander", <String>["Natal", "Outras"]));
-   root.children.add(makeNode("Daewoo", <String>["Porto Alegre", "Outras"]));
-   root.children.add(makeNode("Daihatsu", <String>["Porto Velho", "Outras"]));
-   root.children.add(makeNode("Dodge", <String>["Boa Vista", "Outras"]));
-   root.children.add(makeNode("Effa", <String>["Florianópolis", "Outras"]));
-   root.children.add(makeNode("Engesa", <String>["Sao Paulo", "Outras"]));
-   root.children.add(makeNode("Envemo", <String>["Aracaju", "Outras"]));
-   root.children.add(makeNode("Fiat", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Fibravam", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Ford", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Foton", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Fyber", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Geely", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("GM - Chevrolet", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Greate Wall", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Gurgel", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Hafei", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Honda", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Hyunday", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Isuzu", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Iveco", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Jaca", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Jaguar", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Jeep", <String>["Palmas", "Outras"]));
-   root.children.add(makeNode("Jinbey", <String>["Palmas", "Outras"]));
-   List<MenuNode> ret = List<MenuNode>();
-   ret.add(root);
-   return ret;
-}
